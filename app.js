99f706d8d1de5a706d4787668e78a78875291b9e
salomege <salomegelenidze@gmail.com>
2021-12-11 01:45:53 +0400

    add JS for footer

--- app.js
+++ app.js
@@ -67,3 +67,139 @@ jQuery(function() {
 });
 
 }
+
+
+
+{
+const formValidator = (form, fieldsConfig, onValidateSuccess, onValidationError) => {
+
+  const validateField = (fieldElement, fieldConfig) => {
+    const value = fieldElement.value;
+    const rules = fieldConfig.rules;
+    const formGroup = fieldElement.closest('.form-group');
+    const errorElement = formGroup.querySelector('.form-error-message');
+
+    const fieldValidationResult = {name: fieldConfig.name, value: value, errors: []};
+    rules.forEach(rule => {
+      if (rule.required && !value) {
+        fieldValidationResult.errors.push(rule.message);
+      }
+      if (rule.maxLength && `${value}`.length > rule.maxLength) {
+        fieldValidationResult.errors.push(rule.message);
+      }
+    });
+
+    if(fieldValidationResult.errors.length > 0){
+      errorElement.innerText = fieldValidationResult.errors.join('\n');
+    } else {
+      errorElement.innerText = '';
+    }
+    // console.log(fieldValidationResult);
+
+    return fieldValidationResult;
+  }
+
+  const validateOnChange = () => {
+    fieldsConfig.forEach((fieldConfig) => {
+      const fieldElement = form.querySelector(`[name="${fieldConfig.name}"]`);
+      fieldElement.addEventListener('input', e => {
+        validateField(e.target, fieldConfig);
+      });
+    })
+  }
+
+  const validateOnSubmit = () => {
+    const validatedFields = [];
+    fieldsConfig.forEach((fieldConfig) => {
+      const fieldElement = form.querySelector(`[name="${fieldConfig.name}"]`);
+      validatedFields.push(validateField(fieldElement, fieldConfig));
+    });
+
+    return validatedFields;
+  }
+
+  const listenFormSubmit = () => {
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      console.log('submit');
+      const errors = [];
+      const validationResult = validateOnSubmit();
+      validationResult.forEach(result => {
+        errors.push(...result.errors);
+      });
+      if(errors.length === 0){
+        onValidateSuccess(validationResult);
+      }else {
+        onValidationError(validationResult);
+      }
+      console.log(validationResult);
+    });
+  }
+  listenFormSubmit();
+  validateOnChange();
+}
+
+const fieldsConfig = [
+  {
+    name: 'full_name',
+    rules: [
+      {required: true, message: 'name is required.'},
+    ]
+  },
+  {
+    name: 'email',
+    rules: [
+      {required: true, message: 'mail is required.'},
+    ]
+  },
+  {
+    name: 'Website',
+    rules: [
+      {required: true, message: 'website is required.'},
+    ]
+  },
+  {
+    name: 'message',
+    rules: [
+      {required: true, message: 'message required.'},
+    ]
+  },
+];
+
+
+const form = document.querySelector(`#form`)
+  
+const onFormSubmitSuccess = (fields) => {
+    sendData(fields)
+  console.log(fields)
+}
+const onFormSubmitError = (fields) => {
+  console.log('Error', fields);
+}
+
+formValidator(form, fieldsConfig, onFormSubmitSuccess, onFormSubmitError);
+
+async function sendData(userData){
+  try {
+    const response = await fetch('http://api.kesho.me/v1/user-test/contact', {
+      method: 'post',
+      body: JSON.stringify(userData),
+      headers: {'Content-Type': 'application/json'}
+    });
+    await response.json();
+  }catch (e){
+    console.log('Error - ', e);
+  }
+}
+
+var nextStep = document.querySelector('#nextStep');
+
+nextStep.addEventListener('click', function (e) {
+  e.preventDefault();
+  // Hide first view
+  document.getElementById('my_form').style.display = 'none';
+
+  // Show thank you message element
+  document.getElementById('thank_you').style.display = 'block';
+});
+}    
